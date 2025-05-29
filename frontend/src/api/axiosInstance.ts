1e6c5bb908d263b3755e4abae2fbe4a5aad5c3ed
sobhi69 <mhegazy6371@gmail.com>
2025-05-29 12:17:55 +0300

    change in frontend

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:3070/api",
+    baseURL:import.meta.env.MODE == 'production' ? 'https://social-media-typescript-production.up.railway.app/api' :
+     "http://localhost:3070/api",
     withCredentials:true,
 })
--- frontend/src/components/popup/Popup.tsx
+++ frontend/src/components/popup/Popup.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useCallback, useEffect, useState } from 'react'
+import { FC, ReactNode, useCallback, useEffect } from 'react'
 import { FaXmark } from "react-icons/fa6";
 interface PopupProps {
     isOpen: boolean,
